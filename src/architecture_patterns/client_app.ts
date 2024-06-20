211a000cd466a3ccc38daa038303eeb1ededd909
Vitor Cezar <vitorczayas@gmail.com>
2024-06-20 15:08:02 -0300

    singleton alterado

--- src/architecture_patterns/client_app.ts
+++ src/architecture_patterns/client_app.ts
@@ -10,7 +10,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 /* module to generate requests to service gateway */
-var axios = require('axios');
+const axios = require('axios');
 import { AxiosResponse, AxiosError } from 'axios';
 
 /* fs module to write to file */
@@ -21,7 +21,7 @@ const port = 5003;
 
 /* Template engine configuration */
 app.set('view engine', 'ejs');
-app.set('views', './views'); //This reference is from the execution point
+app.set('views', './views'); // This reference is from the execution point
 
 /* Configuration to read post request parameters */
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,8 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 /* Static files directory configuration */
 app.use(express.static('src/public'));
 
-//app.get('/addForm', addProjectHandlerForm);
-
+// Routes
 app.post('/capitalize', capitalize_text_handler);
 app.post('/persist', persist_name_handler);
 app.get('/', root_client_handler);
@@ -46,6 +45,33 @@ function root_client_handler(req: any, res: any) {
 /* Function to return text persistence interface */
 function persist_client_handler(req: any, res: any) {
     res.render('database_insertion.ejs');
+}
+
+/* Singleton class to handle file writing */
+class FileWriterSingleton {
+    private static instance: FileWriterSingleton;
+    private filePath: string;
+
+    private constructor() {
+        this.filePath = 'data.txt';
+    }
+
+    public static getInstance(): FileWriterSingleton {
+        if (!FileWriterSingleton.instance) {
+            FileWriterSingleton.instance = new FileWriterSingleton();
+        }
+        return FileWriterSingleton.instance;
+    }
+
+    public writeToFile(data: string) {
+        fs.appendFile(this.filePath, data, (err) => {
+            if (err) {
+                console.error('Error writing to file:', err);
+            } else {
+                console.log('Data written to file successfully.');
+            }
+        });
+    }
 }
 
 /* function to perform text capitalization through web service */
@@ -78,64 +104,46 @@ async function capitalize_text_handler(req: any, res: any) {
         });
 }
 
-/* Singleton class to handle file writing */
-class FileWriterSingleton {
-    private static instance: FileWriterSingleton;
-    private filePath: string;
-
-    private constructor() {
-        this.filePath = 'data.txt';
-    }
-
-    public static getInstance(): FileWriterSingleton {
-        if (!FileWriterSingleton.instance) {
-            FileWriterSingleton.instance = new FileWriterSingleton();
-        }
-        return FileWriterSingleton.instance;
-    }
-
-    public writeToFile(data: string) {
-        fs.appendFile(this.filePath, data, (err) => {
-            if (err) {
-                console.error('Error writing to file:', err);
-            } else {
-                console.log('Data written to file successfully.');
-                
-            }
-        });
-    }
-}
-
 /* function to perform text capitalization through web service */
-async function persist_name_handler(req:any,res:any){
+async function persist_name_handler(req: any, res: any) {
     let natureza_chamado = req.body.natureza_chamado;
     let descricao = req.body.descricao;
     let bd_type = req.body.bd_type;
     console.log(natureza_chamado);
     console.log(descricao);
     console.log(bd_type);
-    let url = 'http://localhost:5000/persistence?natureza_chamado='+natureza_chamado+'&descricao='+descricao+'&bd_type='+bd_type;
+
+    let url = `http://localhost:5000/persistence?natureza_chamado=${natureza_chamado}&descricao=${descricao}&bd_type=${bd_type}`;
+    
+    // Get singleton instance of FileWriterSingleton
+    const fileWriter = FileWriterSingleton.getInstance();
+
     axios.get(url)
-            .then((response: AxiosResponse) => {
-                res.render('response.ejs', {service_response: response.data})
-                // Handle successful response
-                console.log('Response status:', response.status);
-                console.log('Response data:', response.data);
-            })
-            .catch((error: AxiosError) => {
-                // Handle error
-                if (error.response) {
+        .then((response: AxiosResponse) => {
+            const data = `Natureza do Chamado: ${natureza_chamado}, Descrição: ${descricao}, BD Type: ${bd_type}\nResponse Status: ${response.status}\nResponse Data: ${JSON.stringify(response.data)}\n\n`;
+            fileWriter.writeToFile(data);
+
+            res.render('response.ejs', { service_response: response.data });
+            // Handle successful response
+            console.log('Response status:', response.status);
+            console.log('Response data:', response.data);
+        })
+        .catch((error: AxiosError) => {
+            if (error.response) {
+                const data = `Natureza do Chamado: ${natureza_chamado}, Descrição: ${descricao}, BD Type: ${bd_type}\nResponse Status: ${error.response.status}\nResponse Data: ${JSON.stringify(error.response.data)}\n\n`;
+                fileWriter.writeToFile(data);
                 // Server responded with some error status code
                 console.error('Response data:', error.response.data);
                 console.error('Response status:', error.response.status);
                 console.error('Response headers:', error.response.headers);
             } else if (error.request) {
                 const data = `Natureza do Chamado: ${natureza_chamado}, Descrição: ${descricao}, BD Type: ${bd_type}\nNo response received\n\n`;
-                
+                fileWriter.writeToFile(data);
                 // no response was sent
                 console.error('Request:', error.request);
             } else {
                 const data = `Natureza do Chamado: ${natureza_chamado}, Descrição: ${descricao}, BD Type: ${bd_type}\nError: ${error.message}\n\n`;
+                fileWriter.writeToFile(data);
                 // Some processing error
                 console.error('Error:', error.message);
             }
@@ -153,7 +161,7 @@ function view_data_handler(req: any, res: any) {
     });
 }
 
-/* Tratador para inicializar a aplicação (escutar as requisições) */
+/* Function to initialize the application (listen to requests) */
 function listenHandler() {
-    console.log(`Escutando na porta ${port}!`);
+    console.log(`Listening on port ${port}!`);
 }
